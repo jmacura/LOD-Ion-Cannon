5cabc632fa0eb28cd37044e41c6cbe2901d9c377
jmacura <macurajan@gmail.com>
2020-04-24 18:04:38 +0200

    Add .editorconfig
    Fix intendation

--- LODIC-GUI.js
+++ LODIC-GUI.js
@@ -1,13 +1,13 @@
 /* JavaScript Document */
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-/** @author jmacura 2020 */
+/** This Source Code Form is subject to the terms of the Mozilla Public
+	* License, v. 2.0. If a copy of the MPL was not distributed with this
+	* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+	* @author jmacura 2020 */
 
 /**
- * Displays button for a following action.
- *
- */
+	* Displays button for a following action.
+	*
+	*/
 function showNextButton() {
 	if ($('#button-query').css('visibility') === 'hidden') {
 		$('#button-query').css('visibility', 'visible')
@@ -17,10 +17,10 @@ function showNextButton() {
 }
 
 /**
- * Updates information block element.
- *
- * @param {String} info Information which should be displayed to the user
- */
+	* Updates information block element.
+	*
+	* @param {String} info Information which should be displayed to the user
+	*/
 function showInfo(info) {
 	var infoBlock = $('#info-block');
 		while(infoBlock.hasChildNodes()) { //remove existing information
